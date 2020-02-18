9bb38d69e06dba3467fcc27a9923445fb48eb6f5
Willian Carvalho <Willian_matheus14@hotmail.com>
2020-02-18 11:31:28 -0300

    Initial commit

---
+++ Entities/Company.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheriterance.Entities
+{
+    class Company : TaxPayer
+    {
+        public int NumberOfEmployees { get; set; }
+
+        public Company()
+        {
+        }
+
+        public Company(int numberOfEmployees, string name, double anualIncome) : base(name, anualIncome)
+        {
+            NumberOfEmployees = numberOfEmployees;
+        }
+
+        public override double Tax()
+        {
+            if(NumberOfEmployees > 10)
+            {
+                return AnualIncome * 0.14; 
+            }
+            return AnualIncome * 0.16;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Inheriterance.Entities;
+using System.Collections.Generic;
+
+namespace Inheriterance
+{
+    class Program
+    {     
+
+        static void Main(string[] args)
+        {
+            List<TaxPayer> list = new List<TaxPayer>();
+
+            Console.Write("Enter the number of tax payers: ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Tax payer #{i} Data:");
+                Console.Write("Individual or company (i/c)? ");
+                char ch = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Anual income: ");
+                double anualIncome = double.Parse(Console.ReadLine());
+                if(ch == 'i')
+                {
+                    Console.Write("Health Expenditures: ");
+                    double healthExpenditures = double.Parse(Console.ReadLine());
+                    list.Add(new Individual(healthExpenditures, name, anualIncome));
+                }
+                else if(ch == 'c')
+                {
+                    Console.Write("Number of employees: ");
+                    int employees = int.Parse(Console.ReadLine());
+                    list.Add(new Company(employees, name, anualIncome));
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("TAXES PAID:");
+            double sum = 0;
+            foreach(TaxPayer payer in list)
+            {
+                Console.WriteLine($"{payer.Name}: ${payer.Tax()}");
+                sum += payer.Tax();
+            }
+
+            Console.WriteLine($"TOTAL TAXES: ${sum}");
+        }
+    }
+}
